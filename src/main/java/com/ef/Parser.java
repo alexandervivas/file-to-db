cdbbd7701bdf996934e706c05586cf8b00e73b67
Alexander Vivas <falexvr@gmail.com>
2019-06-21 02:19:01 +0100

    Reconfigured logging and added database migration

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -3,6 +3,7 @@ package com.ef;
 import com.ef.domain.Configuration;
 import java.util.Arrays;
 import java.util.Optional;
+import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,11 +22,25 @@ public class Parser {
 
       logger.info("Done");
 
+      performMigration();
+
     } else {
 
       logger.error("There was an error reading the arguments");
 
     }
+  }
+
+  private static void performMigration() {
+
+    logger.info("Performing database migration");
+
+    Flyway flyway = Flyway.configure()
+        .dataSource("jdbc:mysql://localhost:3306/wallethub", "root", "wallethub").load();
+    flyway.migrate();
+
+    logger.info("Done");
+
   }
 
 }
